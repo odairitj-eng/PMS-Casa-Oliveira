1c882c970e460c5a25fada20d0656374f766aa9a
Casa OliveiraAdmin <admin@casaoliveira.com>
2026-03-09 00:16:15 -0300

    cleanup: removed emergency route

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("e1c6038d57eb94b5")
+/******/ 		__webpack_require__.h = () => ("afba7f8787319c89")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "18863634828f14f7"; }
+/******/ 		__webpack_require__.h = function() { return "c610b5ad7cad765e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
